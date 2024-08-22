9782a766fa760739e40aa91daa97026fd83f78eb
Keson Lu (呂承憲) <KesonLu1226@gmail.com>
2024-08-22 18:49:05 +0000

    Practice How to Write a Test, So I add a HomeControllerTest.java

--- demo/src/main/java/com/cerc/demo/controller/HomeController.java
+++ demo/src/main/java/com/cerc/demo/controller/HomeController.java
@@ -1,11 +1,12 @@
 package com.cerc.demo.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.web.bind.annotation.RequestParam;
 
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("/")
